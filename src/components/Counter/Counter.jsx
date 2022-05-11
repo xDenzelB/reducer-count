cabcb5c73704c8313c5db9d152296578e72e8e75
Denzel <dbartolaba123@gmail.com>
2022-05-11 15:40:34 -0700

    added in initialValue and switch case

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,29 +1,44 @@
 import { useEffect, useState } from 'react';
 import styles from './Counter.css';
 
-const colors = {
-  yellow: 'rgb(236, 222, 153)',
-  green: 'rgb(52, 211, 153)',
-  red: 'rgb(239, 68, 68)',
-};
+// const colors = {
+//   yellow: 'rgb(236, 222, 153)',
+//   green: 'rgb(52, 211, 153)',
+//   red: 'rgb(239, 68, 68)',
+// };
+const initialValue = {
+  count: 0,
+  color: 'rgb(236, 222, 153)'
+}
 
+function countReducer(countState, action) {
+  switch (action.type) {
+    case 'increment': {
+      return {
+        count: countState.count + 1,
+        color: action.color
+      };
+    }
+  }
+}
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [currentColor, setCurrentColor] = useState(colors.yellow);
 
-  useEffect(() => {
-    if (count === 0) {
-      setCurrentColor(colors.yellow);
-    }
+  // const [count, setCount] = useState(0);
+  // const [currentColor, setCurrentColor] = useState(colors.yellow);
 
-    if (count > 0) {
-      setCurrentColor(colors.green);
-    }
+  // useEffect(() => {
+  //   if (count === 0) {
+  //     setCurrentColor(colors.yellow);
+  //   }
 
-    if (count < 0) {
-      setCurrentColor(colors.red);
-    }
-  }, [count]);
+  //   if (count > 0) {
+  //     setCurrentColor(colors.green);
+  //   }
+
+  //   if (count < 0) {
+  //     setCurrentColor(colors.red);
+  //   }
+  // }, [count]);
 
   const increment = () => {
     setCount((prevState) => prevState + 1);
